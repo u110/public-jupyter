4eef1444da5fc2983d3a9bf6bde8ed3f41027b90
yuu-ito <yu_ito@voyagegroup.com>
2017-04-03 21:02:26 +0900

    wip add python ml

---
+++ python_machine_learning/2.2.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+class Perceptron(object):
+    """ パーセプトロンの分類器
+    params:
+
+    eta: 学習率
+    n_iter: トレーニング回数
+
+
+    attributes:
+
+    w_
+    errors_
+    """
+
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        """ fit train data
+        
+        X:
+        y:
+        """
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+
+    def net_input(self, X):
+        """ total input """
+        return np.dot(X, self.w_[1:] + self.w_[0])
+
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
